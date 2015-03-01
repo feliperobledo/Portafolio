667e26869354f8e51d30d7d50732983ebf9df733
Felipe Robledo <feliperobledo@Felipes-MacBook-Pro.local>
2015-02-28 20:57:32 -0800

    going over some switch statements.

--- Go/hello.go
+++ Go/hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+  "runtime"
 )
 
 const EPSILON float64 = 0.000001
@@ -16,10 +17,7 @@ func Sqrt(x float64) float64 {
   delta := z - prevZ
 
 	for delta>EPSILON || -delta< -EPSILON {
-    fmt.Println("previous z: %v \n",prevZ)
-
 		newZ := z - ( (math.Pow(z,2) - x) / (2 * z))
-    fmt.Println("new z: %v \n",newZ)
 
 		prevZ = z;
 		z = newZ;
@@ -29,29 +27,18 @@ func Sqrt(x float64) float64 {
 }
 
 func main() {
-  //var hello, world string = "hello", "world"
-  //k := "Eily"
-
-  i:= 42
-  f:=float64(i)
-  u:=uint(f)
-
-  fmt.Printf("%T %T %T\n",i,f,u)
-
-  var k = 1 << 1
-  var k_1 = k << 1;
-  fmt.Printf("%v %v\n",k,k_1)
-
-  for i:=0; i < 10; i++{
-    fmt.Printf("%v\n",i)
-  }
-
-  if false{
-    fmt.Printf("Program End\n")
-  } else{
-    fmt.Printf("Trying something\n")
-  }
-
-  fmt.Println(Sqrt(2))
+  fmt.Println("Newton Form value: ",Sqrt(2))
   fmt.Println("Square root value: ",math.Sqrt(2))
+
+  fmt.Print("Go runs on ")
+  switch os := runtime.GOOS; os {
+    case "darwin":
+      fmt.Println("OS X.")
+    case "linux":
+      fmt.Println("Linux.")
+    default:
+      // freebsd, openbsd,
+      // plan9, windows...
+      fmt.Printf("%s.", os)
+    }
 }
