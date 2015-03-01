5d8871c3f5a415496601a3bb7ac42f965f49d56a
Felipe Robledo <feliperobledo@Felipes-MacBook-Pro.local>
2015-02-28 20:39:26 -0800

    Trying out some logic statements in Go

--- Go/hello.go
+++ Go/hello.go
@@ -1,9 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const EPSILON float64 = 0.000001
+
+func Sqrt(x float64) float64 {
+	var z float64 = 1.0
+
+	prevZ := z - ( (math.Pow(z,2) - x) / (2 * z))
+
+	for (prevZ - z)>EPSILON && -(prevZ - z)<EPSILON {
+		newZ := z - ( (math.Pow(z,2) - x) / (2 * z))
+		prevZ = z;
+		z = newZ;
+	}
+	return z
+}
 
 func main() {
-  var hello, world string = "hello", "world"
+  //var hello, world string = "hello", "world"
+  //k := "Eily"
 
-  fmt.Printf(hello,world)
+  i:= 42
+  f:=float64(i)
+  u:=uint(f)
+
+  fmt.Printf("%T %T %T\n",i,f,u)
+
+  var k = 1 << 1
+  var k_1 = k << 1;
+  fmt.Printf("%v %v\n",k,k_1)
+
+  for i:=0; i < 10; i++{
+    fmt.Printf("%v\n",i)
+  }
+
+  if false{
+    fmt.Printf("Program End\n")
+  } else{
+    fmt.Printf("Trying something\n")
+  }
+
+  fmt.Println(Sqrt(2))
 }
