fe6097eb27be4c9d6236fcd0afc7850e0e89d0f3
Felipe Robledo <feliperobledo@Felipes-MacBook-Pro.local>
2015-03-01 20:37:22 -0800

    GOPATH variable now works. Only problem is that the environment.sh file does not work at the moment. I had to set the variable in the .bash_profile

--- Go/environment.sh
+++ Go/environment.sh
@@ -1,6 +1,18 @@
-#export GOPATH=$PATH:/usr/local/go/bin
+#-------------------------------------------------------------------------------
+#File: environment.sh
+#Description: This bash file should be used to set the GOPATH environmental
+#             variable.
+#
+#Note: I have set the GOPATH under .bash_profile since this file still does not
+#      work. I will continue to automate this process without having to
+#      convolute the user's .bash_profile file.
+#
+#-------------------------------------------------------------------------------
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "setting GOPATH to running directory"
 export GOPATH="$DIR"
 echo "GOPATH=$GOPATH"
+
+export PATH="$PATH:$GOPATH/bin"
+echo "added project bin to PATH: $PATH"
