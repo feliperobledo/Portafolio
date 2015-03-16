6d42952f30f2e3f0e21b94e52c425d601691df39
Felipe Robledo <feliperobledo@Felipes-MacBook-Pro.local>
2015-03-15 17:26:36 -0700

    These are some changes I did last time. I worked.

--- Javascript/firstTest.js
+++ Javascript/firstTest.js
@@ -1,1 +1,14 @@
+//The browser doesn't care that they are seperate files or not
 var Person = function(name) { this.name = name}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function helloWorld() {
+  document.getElementById("demo").innerHTML = "Hello World.";
+}
+
+function logTest() {
+  console.log(5 + 6);
+}
