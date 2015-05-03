f8743abbfd957d71b2191b74c06d11e35109b939
Felipe Robledo <feliperobledo@felipes-mbp.attlocal.net>
2015-05-02 19:06:19 -0700

    deferred statements are pretty weird. I wonder what I can really do with them that I can't do in regular C/C++...

--- Go/src/frPackage/packge_1.go
+++ Go/src/frPackage/packge_1.go
@@ -1,8 +1,53 @@
-//this is just a test package
+// this is just a test package
 package frPackage
 
-//print a simple string as a test
+import (
+  "fmt"
+	"math"
+  "runtime"
+)
+
+
+// Package Constants
+const EPSILON float64 = 0.000001
+
+// print a simple string as a test
 func ToPrint() (ret string) {
   ret ="hello from my first package"
   return
+}
+
+func GetOSVersion() {
+  fmt.Print("Go runs on ")
+  switch os := runtime.GOOS; os {
+    case "darwin":
+      fmt.Println("OS X.")
+      fallthrough // This is nifty. This is the default behavior in C/C++. Here,
+                  //    I need this statement for tto actually happen.
+    case "linux":
+      fmt.Println("Linux.")
+      fallthrough
+    default:
+      // freebsd, openbsd,
+      // plan9, windows...
+      fmt.Printf("%s.", os)
+    }
+}
+
+func Sqrt(x float64) float64 {
+  // With the Newton method, the lower the z value is,
+  // the closer to the square root
+	var z float64 = 1.0
+
+	prevZ := 0.0
+  delta := z - prevZ
+
+	for delta>EPSILON || -delta< -EPSILON {
+		newZ := z - ( (math.Pow(z,2) - x) / (2 * z))
+
+		prevZ = z;
+		z = newZ;
+    delta = z - prevZ;
+	}
+	return z
 }
--- Go/src/startProject/start.go
+++ Go/src/startProject/start.go
@@ -3,46 +3,19 @@ package main
 import (
 	"fmt"
 	"math"
-  "runtime"
 	"frPackage"
 )
 
-const EPSILON float64 = 0.000001
-
-func Sqrt(x float64) float64 {
-  //With the Newton method, the lower the z value is,
-  //the closer to the square root
-	var z float64 = 1.0
-
-	prevZ := 0.0
-  delta := z - prevZ
-
-	for delta>EPSILON || -delta< -EPSILON {
-		newZ := z - ( (math.Pow(z,2) - x) / (2 * z))
-
-		prevZ = z;
-		z = newZ;
-    delta = z - prevZ;
-	}
-	return z
-}
-
 func main() {
-  fmt.Println("Newton Form value: ",Sqrt(2))
+	defer fmt.Println("This statement was deferred.\n\n")
+
+  fmt.Println("Newton Form value: ",frPackage.Sqrt(2))
   fmt.Println("Square root value: ",math.Sqrt(2))
 
-  fmt.Print("Go runs on ")
-  switch os := runtime.GOOS; os {
-    case "darwin":
-      fmt.Println("OS X.")
-    case "linux":
-      fmt.Println("Linux.")
-    default:
-      // freebsd, openbsd,
-      // plan9, windows...
-      fmt.Printf("%s.", os)
-    }
+  frPackage.GetOSVersion();
 
-		//sample test on test package
-		fmt.Printf("Reversed sample: %s \n",frPackage.Reverse(frPackage.ToPrint()))
+  //sample test on test package
+  fmt.Printf("Reversed sample: %s \n",frPackage.Reverse(frPackage.ToPrint()))
+
+	fmt.Println("\n\nBut this was not.")
 }
