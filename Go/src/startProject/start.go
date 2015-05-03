34dadb90433a9da6f9e34fc6026e9af3def2e6f3
Felipe Robledo <feliperobledo@felipes-mbp.attlocal.net>
2015-05-02 19:21:49 -0700

    This commit is broken because it's a reminder that Go does not allow one to have function pointers. Then how do they handle jump tables?

--- Go/src/startProject/start.go
+++ Go/src/startProject/start.go
@@ -7,7 +7,17 @@ import (
 )
 
 func main() {
-	defer fmt.Println("This statement was deferred.\n\n")
+	// defer pushes the function onto a stack, and the stack is popped when
+	//     leaving the current function's scope. It is as if they are actually
+	//     pushing the function calls onto this function's stack, and poppping them
+	//     at the end.
+	{
+		// This does not even pop it at the end of this scope, and yet this is valid
+		//    Go code.
+		defer fmt.Println("This statement was deferred.\n\n")
+	}
+	// Cannot take the address of a function :(
+  fPtr := &(fmt.Println);
 
   fmt.Println("Newton Form value: ",frPackage.Sqrt(2))
   fmt.Println("Square root value: ",math.Sqrt(2))
