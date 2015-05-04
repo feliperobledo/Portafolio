3b2b2259b78ae4434ecac41217a18e67c46e9762
Felipe Robledo <feliperobledo@felipes-mbp.attlocal.net>
2015-05-03 17:05:44 -0700

    So I changed the 2D points file to implement an interface. However, now I see that instead of writing my own printing functions, I could have implemented the Stringer interface's String() string function to let stdout know how to print a vertex. That is kind of neat :). We can have interfaces in 1 file, and anything else that implements it anywhere else. However, doesn't this increase compile time? Is it not hard for the linker to verify that all the dependencies between types and interface implemented methods? This seems like a big can of worms.

--- Go/src/Math2D/PointMath.go
+++ Go/src/Math2D/PointMath.go
@@ -5,11 +5,13 @@ import (
   //"math"
 )
 
+// 2DPoint types================================================================
 type Point2D_int struct {
   X int
   Y int
 }
 
+type P2int Point2D_int;
 type Point2DArray []Point2D_int;
 
 type Point2D_float struct {
@@ -17,23 +19,41 @@ type Point2D_float struct {
   Y float64
 }
 
-func Point2D_Int_New(x int, y int) *Point2D_int {
-  return &Point2D_int{x,y};
+type P2float Point2D_float;
+type f64Point2DArray []Point2D_float;
+
+// Interface for different 2D point types to implement==========================
+type IPoint2D interface {
+  PrintPoint();
+  Add(rhs *P2int) *P2int;
+}
+
+
+// 2DPoint type allocation methods==============================================
+func Point2D_Int_New(x int, y int) *P2int {
+  return &P2int{x,y};
 }
 
 func Point2D_Int_Array(count int) Point2DArray {
   return make([]Point2D_int,count);
 }
 
-// Integer Point Methods
-
-func (in *Point2D_int) Point2D_Int_Print() {
-  fmt.Println("P(",in.X,",",in.Y,")\n")
+// 2DPoint - int interface implementation=======================================
+func (this *P2int) PrintPoint() {
+  fmt.Println("P(",this.X,",",this.Y,")\n")
 }
 
-func (x Point2DArray) Point2D_Int_PrintArray() {
+func (this *P2int) Add(rhs *P2int) *P2int {
+  this.X += rhs.X;
+  this.Y += rhs.Y;
+  return this;
+}
+
+func (x Point2DArray) PrintPoint() {
   fmt.Printf("len=%d cap=%d %v\n", len(x), cap(x), x);
 }
+
+
 
 func Point2D_Int_Add(out *Point2D_int,in1 Point2D_int, in2 Point2D_int) {
   (*out).X = in1.X + in2.X
--- Go/src/startProject/start.go
+++ Go/src/startProject/start.go
@@ -9,18 +9,18 @@ import (
 func main() {
 	fmt.Println("Testing structs and pointers");
 
-	var p1 Math2D.Point2D_int;
-	(&p1).Point2D_Int_Print();
+	var p1 Math2D.P2int;
+	(&p1).PrintPoint();
 
   // The return type of the new function is a pointer on the heap
 	var p2 = Math2D.Point2D_Int_New(5,5);
-	p2.Point2D_Int_Print();
+	p2.PrintPoint();
 
 	// The return type is a slice/array of vertices.
 	// According to the documentation, a slice is a pointer to an array.
 	// According to the doucmentation, a slice is pertty much a C++ vector
 	var pArray Math2D.Point2DArray = Math2D.Point2D_Int_Array(5);
-	pArray.Point2D_Int_PrintArray();
+	pArray.PrintPoint();
 
 	// According to the documentation, there is also such a thing as maps, which
 	//   is your standard hash-map
