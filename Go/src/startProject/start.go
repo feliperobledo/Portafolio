c0cf02489b2e4c8f04d248301809899293b3e5cc
Felipe Robledo <gamedev_fantasy@hotmail.com>
2015-07-07 21:54:18 -0700

    removed some code

--- Go/src/startProject/start.go
+++ Go/src/startProject/start.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	"SimpleEchoServer"
 	//"time"
 	//"Math2D"
 )
 
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		//time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
-	}
-}
-
 func main() {
-	go say("world")
-	say("hello")
+	echoServer.Server();
 }
